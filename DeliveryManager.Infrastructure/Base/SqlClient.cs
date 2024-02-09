9781515358b73fbde651c644c106c4a11c8da543
Ana-Vega05 <143562484+Ana-Vega05@users.noreply.github.com>
2024-02-09 00:07:44 -0500

    Behavior was given to the three methods to connect the project with the database

---
+++ DeliveryManager.Infrastructure/Base/SqlClient.cs
@@ -0,0 +1,34 @@
+using Dapper;
+using DeliveryManager.Domain.Repositories;
+using Npgsql;
+
+namespace DeliveryManager.Infrastructure.Base;
+
+public class SqlClient : ISqlClient
+{
+    private string connectionString = "Host=localhost;Port=5432;Database=postgres;" +
+                                      "User ID=postgres;Password=123456";
+    public async Task<List<T>> ExecuteQuery<T>(string query, DynamicParameters? parameters = null)
+    {
+        var connection = new NpgsqlConnection(connectionString);
+        var response  = await connection.QueryAsync<T>(query, parameters );
+        await connection.CloseAsync();
+        return response.ToList();
+    }
+    
+
+    public async Task<T?> ExecuteQuerySingle<T>(string query, DynamicParameters? parameters = null)
+    {
+        var connection = new NpgsqlConnection(connectionString);
+        var response = await connection.QuerySingleAsync<T>(query, parameters);
+        await connection.CloseAsync();
+        return response;
+    }
+
+    public async Task ExecuteCommand(string command, object? parameters = null)
+    {
+        var connection = new NpgsqlConnection(connectionString);
+        await connection.ExecuteAsync(command, parameters);
+        await connection.CloseAsync();
+    }
+}
